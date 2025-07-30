4b4433ac3cecd4ddf4a7110f68f7fbc18c34c22a
canine89 <hgpark@goldenrabbit.co.kr>
2025-07-30 11:31:07 +0900

    Add Disqus comments system to blog posts

--- src/app/posts/[...slug]/page.tsx
+++ src/app/posts/[...slug]/page.tsx
@@ -6,6 +6,7 @@ import { Metadata } from 'next'
 import { siteConfig } from '@/lib/config'
 import Link from 'next/link'
 import { SocialShare } from '@/components/SocialShare'
+import { Disqus } from '@/components/Disqus'
 
 interface PostPageProps {
   params: Promise<{
@@ -180,6 +181,13 @@ export default async function PostPage({ params }: PostPageProps) {
             </Link>
           </div>
         </nav>
+
+        {/* Disqus Comments */}
+        <Disqus 
+          identifier={id}
+          title={post.title}
+          url={postUrl}
+        />
       </article>
     </>
   )
