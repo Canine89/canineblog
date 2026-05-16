84355081bc921b19367c1f0d589285573864e483
canine89 <hgpark@goldenrabbit.co.kr>
2026-05-16 22:49:26 +0900

    fix: 홈 프로필 소셜 링크 설정 추가

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,17 +1,19 @@
 export const siteConfig = {
   // 블로그 기본 정보
-  title: "편집자P의 편집실",
+  title: "편집자P의 AI/AGENT 편집실",
   description: "공부하고, 쓰고, 익혀라",
 
   // 블로그 주인 정보
   author: {
     name: "편집자P",
     email: "hgpark@goldenrabbit.co.kr",
-    bio: "개발이 취미인 컴공과 출신 IT 도서 기획/편집자. 활동명 편집자P로 더 많이 알려져 있다. 사내에서 사용하는 각종 자동화 앱을 파이썬, 자바스크립트로 개발하여 적극 활용하고 있다. IT 지식을 더 쉽게 나누기 위해 책을 쓰고, 유튜브와 쇼츠를 제작한다.",
+    bio: "개발이 취미인 컴공과 출신 IT 도서 기획/편집자이자 IT 애호가. 활동명 편집자P로 더 많이 알려져 있습니다. 사내에서 사용하는 각종 자동화 앱을 파이썬, 자바스크립트로 개발하여 적극 활용하고 있습니다. IT 지식을 더 쉽게 나누기 위해 책을 쓰고, 유튜브와 쇼츠를 제작하며, ai100.co.kr 홈페이지를 운영하고 있습니다.",
     avatar: "/avatar.jpg", // 프로필 이미지가 없으면 null로 설정
     social: {
       github: "https://github.com/canine89",
       threads: "https://www.threads.com/@limedaddy_8924",
+      youtube: "https://www.youtube.com/@editorp89",
+      instructor: "https://docs.google.com/document/d/1bZ1TlO8acV-tytns-vXeQ_EP_RXNXvG2kYiKZQzku8g/edit?tab=t.0",
     }
   },
 
